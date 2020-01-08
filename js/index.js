f9a75b344a08de3c07eabe52e17bd3dcaa28fac0
Haqz <maszynista91@gmail.com>
2020-01-08 22:38:16 +0100

    Changed header style and added js functionality

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    var play = document.getElementById("1");
+    var packs = document.getElementById("2");
+    var mods = document.getElementById("3");
+    var settings = document.getElementById("4");
+    play.onclick = function(){
+        console.log("reeeeeee");
+    }
+    packs.onclick = function(){
+        console.log("reeeeeee");
+    }
+    mods.onclick = function(){
+        console.log("reeeeeee");
+    }
+    settings.onclick = function(){
+        console.log("reeeeeee");
+    }
+});
